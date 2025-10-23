adc55dc7258f8524de12c269718822e0875b18da
chinmay joshi <110737976+chinmay1243h@users.noreply.github.com>
2025-10-23 10:46:46 +0530

    update
    update

--- client/src/services/taskService.js
+++ client/src/services/taskService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/tasks";
+const API_URL = `${process.env.REACT_APP_API_URL}/api/tasks`;
 
 // ✅ Function to get the token from localStorage
 const getAuthConfig = () => {
